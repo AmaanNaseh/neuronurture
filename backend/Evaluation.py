ddc3d9d64566354783788747ea5dc639280a3938
AmaanNaseh <naseh.amaan@gmail.com>
2024-08-25 13:30:09 +0530

    Finalized Code

---
+++ backend/Evaluation.py
@@ -0,0 +1,47 @@
+from pandas import read_csv
+from matplotlib import pyplot
+
+# 6 ML Algorithm
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+
+dataset = read_csv("train.csv")
+print(dataset.shape)
+
+array = dataset.values
+X = array[:,0:9]
+y = array[:,10]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1, shuffle=True)
+
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+
+results = []
+names = []
+res = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=None)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    res.append(cv_results.mean())
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+pyplot.ylim(.830, .890)
+pyplot.bar(names, res, color ='maroon', width = 0.6)
+pyplot.title('Algorithm Comparison')
+pyplot.show()
